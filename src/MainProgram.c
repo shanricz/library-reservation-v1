13e45cbd1b5a29da9de346b7a1f235ff8eaefd5c
shanricz <ilao.shanriczbscs2025@gmail.com>
2025-09-27 14:50:36 +0800

    Initialize Main

--- src/MainProgram.c
+++ src/MainProgram.c
@@ -2,20 +2,19 @@
 #include "headers/database.h"
 #include "headers/reservation.h"
 
+sqlite3 *db;
+
 int main() {
 
     printf("--->LIBRARY CONSULTATION ROOM RESERVATION SYSTEM <---\n");
     printf("Initializing system...\n\n");
 
-    sqlite3 *db;
-    int rc = sqlite3_open("test.db", &db);
-    if (rc) {
-        printf("Can't open database: %s\n", sqlite3_errmsg(db));
+    //Initialize db
+    if (init_database() != 0) {
+        printf("Error: Failed to initialize database. Exiting...\n");
+        pause_screen();
         return 1;
-    } else {
-        printf("Opened database successfully\n");
     }
-    sqlite3_close(db);
     return 0;
 }
 
